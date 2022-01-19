fd4f20c7989f2c778be3e8aeaeff4b39302b514b
Sergio Martin <eienburuu@gmail.com>
2022-01-18 21:12:49 -0800

    Adding w402b

---
+++ examples/w402/w402a.sol
@@ -0,0 +1,1 @@
+. . . . . . . . . . . . . . . . . R R R R R R R R R R R R R R R R R R R R R R R R R R R R R R R R R R LR RB LR LR R L RA RA RA R R R R R R R R R R R R R R R R R R R R R RB RA RA RA RA RA RA RA RA RA RA RA RA A RA R R R R R R R R R R L R R R . L R L R L R . R . . R RB A A RA A RA RA RA A RA RA RA A A A RA RA A R R R . R R R R . R R R R . R . . . . R . R . R L R RB R
