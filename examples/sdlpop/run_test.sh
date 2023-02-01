bc7df2b863ae48ecc448b3268243053d8cea7b8a
Sergio Martin <martiser@ethz.ch>
2023-02-01 20:31:52 +0000

    More progress with solver

--- examples/sdlpop/run_test.sh
+++ examples/sdlpop/run_test.sh
@@ -1,1 +1,1 @@
-python3 ./runner.py 0800 --dbMinSize=40000 --dbMaxSize=70000 --hashDBCount=2 --hashDBSize=20000
+python3 ./runner.py 0600 --dbMinSize=400 --dbMaxSize=7000 --hashDBCount=2 --hashDBSize=10000
