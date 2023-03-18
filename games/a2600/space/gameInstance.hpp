699b9c2c5ea84a57c34ec8beb1e31cbff713f1df
Sergio Martin <martiser@ethz.ch>
2023-03-18 07:41:38 +0000

    Adding barnstorming and space invaderrs

---
+++ games/a2600/space/gameInstance.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#define _INVERSE_FRAME_RATE 16667
+
+#include "gameInstanceBase.hpp"
+#include <set>
+
+// Datatype to describe a generic magnet
+struct genericMagnet_t {
+ float intensity = 0.0; // How strong the magnet is
+ float center = 0.0;  // What is the central point of attraction
+};
+
+// Datatype to describe a magnet
+struct magnetSet_t {
+};
+
+class GameInstance : public GameInstanceBase
+{
+ public:
+
+  uint8_t* gameMode;
+  uint8_t* gameTimer;
+  uint8_t* score1;
+  uint8_t* score2;
+  uint8_t* bullet1PosY;
+  uint8_t* bullet2PosY;
+
+  // Container for game-specific values
+  uint8_t timerTolerance;
+
+  // Derivative Values
+  float score;
+
+  std::vector<INPUT_TYPE> advanceGameState(const INPUT_TYPE &move) override;
+  GameInstance(EmuInstance* emu, const nlohmann::json& config);
+  _uint128_t computeHash() const override;
+  void updateDerivedValues() override;
+  std::vector<std::string> getPossibleMoves(const bool* rulesStatus) const override;
+  magnetSet_t getMagnetValues(const bool* rulesStatus) const;
+  float getStateReward(const bool* rulesStatus) const override;
+  void printStateInfo(const bool* rulesStatus) const override;
+};
