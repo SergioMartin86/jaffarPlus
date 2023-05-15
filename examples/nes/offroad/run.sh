b7ba4455d23b33b1fa42114f1b7301e11ce87322
Sergio Martin <eienburuu@gmail.com>
2023-05-15 21:47:25 +0300

    changes

--- examples/nes/offroad/run.sh
+++ examples/nes/offroad/run.sh
@@ -10,16 +10,16 @@ do
   if [ $? -ne 0 ]; then echo "Failed to copy base ${i}"; exit -1; fi
   jaffar race.jaffar
   if [ $? -ne 0 ]; then echo "Failed to solve race ${i}"; exit -1; fi
-  cp /tmp/jaffar.best.sol sols/race.sol.${i}
+  cp jaffar.best.sol sols/race.sol.${i}
   if [ $? -ne 0 ]; then echo "Failed to copy solution for race ${i}"; exit -1; fi
-  cp /tmp/jaffar.best.state skip.state
+  cp jaffar.best.state skip.state
   if [ $? -ne 0 ]; then echo "Failed to copy state for race ${i}"; exit -1; fi
-  cp /tmp/jaffar.best.state skip.state.${i}
+  cp jaffar.best.state skip.state.${i}
   if [ $? -ne 0 ]; then echo "Failed to copy state for race ${i}"; exit -1; fi
   jaffar skip.jaffar
   if [ $? -ne 0 ]; then echo "Failed to skip to next race ${i}"; exit -1; fi
-  cp /tmp/jaffar.best.sol sols/skip.sol.${i}
+  cp jaffar.best.sol sols/skip.sol.${i}
   if [ $? -ne 0 ]; then echo "Failed to copy solution for skip ${i}"; exit -1; fi
-  cp /tmp/jaffar.best.state race.state
+  cp jaffar.best.state race.state
   if [ $? -ne 0 ]; then echo "Failed to copy state for race ${i}"; exit -1; fi
 done
