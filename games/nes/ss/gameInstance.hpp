a26f75303285b5d7526bd6a5ac8753ed12fac70c
Sergio Martin <sergio.miguel.martin@huawei.com>
2023-06-24 00:48:10 -0400

    Adding saint seiya

---
+++ games/nes/ss/gameInstance.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#define _INVERSE_FRAME_RATE 16667
+
+#include "gameInstanceBase.hpp"
+#include <set>
+
+// Datatype to describe a generic magnet
+struct genericMagnet_t {
+ float intensity = 0.0; // How strong the magnet is
+ float center = 0.0;  // What is the central point of attraction
+};
+
+// Datatype to describe a magnet
+struct magnetSet_t {
+ genericMagnet_t screenHorizontalMagnet;
+ genericMagnet_t playerHorizontalMagnet;
+ genericMagnet_t playerVerticalMagnet;
+};
+
+class GameInstance : public GameInstanceBase
+{
+ public:
+
+  // Container for game-specific values
+  uint8_t* gameMode           ;
+  uint8_t* screenPosX1        ;
+  uint8_t* screenPosX2        ;
+  uint8_t* playerPosX         ;
+  uint8_t* playerPosY         ;
+  uint8_t* playerJumpStrength ;
+  uint8_t* playerAttackState  ;
+  uint8_t* playerAction       ;
+  uint8_t* playerDirection    ;
+  uint8_t* playerHP1          ;
+  uint8_t* playerHP2          ;
+  uint8_t* playerMP1          ;
+  uint8_t* playerMP2          ;
+  uint8_t* ShunMP1            ;
+  uint8_t* ShunMP2            ;
+  uint8_t* ShunHP1            ;
+  uint8_t* ShunHP2            ;
+
+  uint8_t timerTolerance;
+
+  float screenPosX;
+  float realPlayerPosX;
+
+  std::vector<INPUT_TYPE> advanceGameState(const INPUT_TYPE &move) override;
+  GameInstance(EmuInstance* emu, const nlohmann::json& config);
+  _uint128_t computeHash(const uint16_t currentStep = 0) const override;
+  void updateDerivedValues() override;
+  std::vector<std::string> getPossibleMoves(const bool* rulesStatus) const override;
+  magnetSet_t getMagnetValues(const bool* rulesStatus) const;
+  float getStateReward(const bool* rulesStatus) const override;
+  void printStateInfo(const bool* rulesStatus) const override;
+};
