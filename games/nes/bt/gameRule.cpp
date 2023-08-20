af467d1189e7166150595f5c06e6e06f6fa568c2
Sergio Martin <martiser@ethz.ch>
2023-08-20 08:24:56 +0000

    More progress

--- games/nes/bt/gameRule.cpp
+++ games/nes/bt/gameRule.cpp
@@ -20,6 +20,10 @@ datatype_t GameRule::getPropertyType(const nlohmann::json& condition)
   if (propertyName == "Key Address 1") return dt_uint8;
   if (propertyName == "Key Address 2") return dt_uint8;
   if (propertyName == "Key Address 3") return dt_uint8;
+  if (propertyName == "Player 1 Lives") return dt_uint8;
+  if (propertyName == "Key Event 1 Triggered") return dt_uint8;
+  if (propertyName == "Key Event 2 Triggered") return dt_uint8;
+  if (propertyName == "Lag Frame") return dt_uint8;
 
   EXIT_WITH_ERROR("[Error] Rule %lu, unrecognized property: %s\n", _label, propertyName.c_str());
 
@@ -34,6 +38,10 @@ void* GameRule::getPropertyPointer(const nlohmann::json& condition, GameInstance
   if (propertyName == "Key Address 1") return gameInstance->keyAddress1;
   if (propertyName == "Key Address 2") return gameInstance->keyAddress2;
   if (propertyName == "Key Address 3") return gameInstance->keyAddress3;
+  if (propertyName == "Player 1 Lives") return gameInstance->player1Lives;
+  if (propertyName == "Key Event 1 Triggered") return gameInstance->keyEvent1Triggered;
+  if (propertyName == "Key Event 2 Triggered") return gameInstance->keyEvent2Triggered;
+  if (propertyName == "Lag Frame") return gameInstance->lagFrame;
 
   EXIT_WITH_ERROR("[Error] Rule %lu, unrecognized property: %s\n", _label, propertyName.c_str());
 
