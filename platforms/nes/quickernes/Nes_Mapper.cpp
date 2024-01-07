6ca5724ca496a80d78e97ec8d9599c18e6dc0cfa
Sergio Martin <eienburuu@gmail.com>
2024-01-07 14:07:30 +0100

    In the middle of re-adding mappers

--- platforms/nes/quickernes/Nes_Mapper.cpp
+++ platforms/nes/quickernes/Nes_Mapper.cpp
@@ -5,8 +5,62 @@
 #include <string.h>
 #include "Nes_Core.h"
 #include <cstdio>
-#include "mappers/mapper_002_Unrom.hpp"
-#include "mappers/mapper_071_Camerica.hpp"
+
+#include "mappers/mapper000.hpp"
+#include "mappers/mapper001.hpp"
+#include "mappers/mapper002.hpp"
+#include "mappers/mapper003.hpp"
+#include "mappers/mapper004.hpp"
+#include "mappers/mapper005.hpp"
+#include "mappers/mapper007.hpp"
+#include "mappers/mapper009.hpp"
+#include "mappers/mapper010.hpp"
+#include "mappers/mapper011.hpp"
+#include "mappers/mapper015.hpp"
+#include "mappers/mapper019.hpp"
+#include "mappers/mapper021.hpp"
+#include "mappers/mapper022.hpp"
+#include "mappers/mapper023.hpp"
+#include "mappers/mapper024.hpp"
+#include "mappers/mapper025.hpp"
+#include "mappers/mapper026.hpp"
+// #include "mappers/mapper030.hpp"
+// #include "mappers/mapper032.hpp"
+// #include "mappers/mapper033.hpp"
+// #include "mappers/mapper034.hpp"
+// #include "mappers/mapper060.hpp"
+// #include "mappers/mapper066.hpp"
+// #include "mappers/mapper069.hpp"
+// #include "mappers/mapper070.hpp"
+// #include "mappers/mapper071.hpp"
+// #include "mappers/mapper073.hpp"
+// #include "mappers/mapper075.hpp"
+// #include "mappers/mapper078.hpp"
+// #include "mappers/mapper079.hpp"
+// #include "mappers/mapper085.hpp"
+// #include "mappers/mapper086.hpp"
+// #include "mappers/mapper087.hpp"
+// #include "mappers/mapper088.hpp"
+// #include "mappers/mapper089.hpp"
+// #include "mappers/mapper093.hpp"
+// #include "mappers/mapper094.hpp"
+// #include "mappers/mapper097.hpp"
+// #include "mappers/mapper113.hpp"
+// #include "mappers/mapper140.hpp"
+// #include "mappers/mapper152.hpp"
+// #include "mappers/mapper154.hpp"
+// #include "mappers/mapper156.hpp"
+// #include "mappers/mapper180.hpp"
+// #include "mappers/mapper184.hpp"
+// #include "mappers/mapper190.hpp"
+// #include "mappers/mapper193.hpp"
+// #include "mappers/mapper206.hpp"
+// #include "mappers/mapper207.hpp"
+// #include "mappers/mapper232.hpp"
+// #include "mappers/mapper240.hpp"
+// #include "mappers/mapper241.hpp"
+// #include "mappers/mapper244.hpp"
+// #include "mappers/mapper246.hpp"
 
 /* Copyright (C) 2004-2006 Shay Green. This module is free software; you
 can redistribute it and/or modify it under the terms of the GNU Lesser
@@ -162,13 +216,6 @@ int Nes_Mapper::handle_bus_conflict( nes_addr_t addr, int data )
 }
 #endif
 
-	// { 0, Nes_Mapper::make_nrom },
-	// { 1, Nes_Mapper::make_mmc1 },
-	// { 2, Nes_Mapper::make_unrom },
-	// { 3, Nes_Mapper::make_cnrom },
-	// { 4, Nes_Mapper::make_mmc3 },
-	// { 7, Nes_Mapper::make_aorom }
-
 Nes_Mapper* Nes_Mapper::create( Nes_Cart const* cart, Nes_Core* emu )
 {
   // Getting cartdrige mapper code
@@ -178,8 +225,61 @@ Nes_Mapper* Nes_Mapper::create( Nes_Cart const* cart, Nes_Core* emu )
   Nes_Mapper* mapper = NULL;
 
   // Now checking if the detected mapper code is supported
-  if (mapperCode ==  2) mapper = new Mapper_002_Unrom();
-  if (mapperCode == 71) mapper = new Mapper_071_Camerica();
+  if (mapperCode ==   0) mapper = new Mapper000();
+  if (mapperCode ==   1) mapper = new Mapper001();
+  if (mapperCode ==   2) mapper = new Mapper002();
+  if (mapperCode ==   3) mapper = new Mapper003();
+  if (mapperCode ==   4) mapper = new Mapper004();
+  if (mapperCode ==   5) mapper = new Mapper005();
+  if (mapperCode ==   7) mapper = new Mapper007();
+  if (mapperCode ==   9) mapper = new Mapper009();
+  if (mapperCode ==  10) mapper = new Mapper010();
+  if (mapperCode ==  11) mapper = new Mapper011();
+  if (mapperCode ==  15) mapper = new Mapper015();
+  if (mapperCode ==  19) mapper = new Mapper019();
+  if (mapperCode ==  21) mapper = new Mapper021();
+  if (mapperCode ==  22) mapper = new Mapper022();
+  if (mapperCode ==  23) mapper = new Mapper023();
+  if (mapperCode ==  24) mapper = new Mapper024();
+  if (mapperCode ==  25) mapper = new Mapper025();
+  if (mapperCode ==  26) mapper = new Mapper026();
+//   if (mapperCode ==  30) mapper = new Mapper030();
+//   if (mapperCode ==  32) mapper = new Mapper032();
+//   if (mapperCode ==  33) mapper = new Mapper033();
+//   if (mapperCode ==  34) mapper = new Mapper034();
+//   if (mapperCode ==  60) mapper = new Mapper060();
+//   if (mapperCode ==  66) mapper = new Mapper066();
+//   if (mapperCode ==  69) mapper = new Mapper069();
+//   if (mapperCode ==  70) mapper = new Mapper070();
+//   if (mapperCode ==  71) mapper = new Mapper071();
+//   if (mapperCode ==  73) mapper = new Mapper073();
+//   if (mapperCode ==  75) mapper = new Mapper075();
+//   if (mapperCode ==  78) mapper = new Mapper078();
+//   if (mapperCode ==  79) mapper = new Mapper079();
+//   if (mapperCode ==  85) mapper = new Mapper085();
+//   if (mapperCode ==  86) mapper = new Mapper086();
+//   if (mapperCode ==  87) mapper = new Mapper087();
+//   if (mapperCode ==  88) mapper = new Mapper088();
+//   if (mapperCode ==  89) mapper = new Mapper089();
+//   if (mapperCode ==  93) mapper = new Mapper093();
+//   if (mapperCode ==  94) mapper = new Mapper094();
+//   if (mapperCode ==  97) mapper = new Mapper097();
+//   if (mapperCode == 113) mapper = new Mapper113();
+//   if (mapperCode == 140) mapper = new Mapper140();
+//   if (mapperCode == 152) mapper = new Mapper152();
+//   if (mapperCode == 154) mapper = new Mapper154();
+//   if (mapperCode == 156) mapper = new Mapper156();
+//   if (mapperCode == 180) mapper = new Mapper180();
+//   if (mapperCode == 184) mapper = new Mapper184();
+//   if (mapperCode == 190) mapper = new Mapper190();
+//   if (mapperCode == 193) mapper = new Mapper193();
+//   if (mapperCode == 206) mapper = new Mapper206();
+//   if (mapperCode == 207) mapper = new Mapper207();
+//   if (mapperCode == 232) mapper = new Mapper232();
+//   if (mapperCode == 240) mapper = new Mapper240();
+//   if (mapperCode == 241) mapper = new Mapper241();
+//   if (mapperCode == 244) mapper = new Mapper244();
+//   if (mapperCode == 246) mapper = new Mapper246();
 
   // If no mapper was found, return null (error) now 
   if (mapper == NULL)
