c3f95a7388b7d70ab95f671d7c31dd86cd196c51
Sergio Martin <eienburuu@gmail.com>
2024-01-29 20:16:32 +0100

    Progress with tester

--- source/tester.cpp
+++ source/tester.cpp
@@ -13,6 +13,10 @@ int main(int argc, char *argv[])
     .help("path to the Jaffar configuration script (.jaffar) file to run.")
     .required();
 
+  program.add_argument("--cycleType")
+    .help("Specifies the emulation actions to be performed per each input. Possible values: 'Simple': performs only advance state, 'Rerecord': performs load/advance/save, and 'Full': performs load/advance/save/advance.")
+    .default_value(std::string("Simple"));
+
   program.add_argument("solutionFile")
     .help("path to the solution sequence file (.sol) to reproduce.")
     .required();
@@ -22,27 +26,37 @@ int main(int argc, char *argv[])
   catch (const std::runtime_error &err) { EXIT_WITH_ERROR("%s\n%s", err.what(), program.help().str().c_str()); }
 
   // Parsing config file
-  std::string configFile = program.get<std::string>("configFile");
+  const std::string configFile = program.get<std::string>("configFile");
   std::string configFileString;
   if (jaffarPlus::loadStringFromFile(configFileString, configFile) == false) 
     EXIT_WITH_ERROR("[ERROR] Could not find or read from Jaffar config file: %s\n%s \n", configFile.c_str(), program.help().str().c_str());
 
   // If sequence file defined, load it and play it
-  std::string solutionFile = program.get<std::string>("solutionFile");
+  const std::string solutionFile = program.get<std::string>("solutionFile");
   std::string solutionFileString;
   if (jaffarPlus::loadStringFromFile(solutionFileString, solutionFile) == false) 
     EXIT_WITH_ERROR("[ERROR] Could not find or read from solution sequence file: %s\n%s \n", solutionFile.c_str(), program.help().str().c_str());
 
+  // Getting reproduce flag
+  const std::string cycleType = program.get<std::string>("--cycleType");
+ 
+  // Parsing cycleType
+  bool recognizedCycleType = false;
+  if (cycleType == "Simple") recognizedCycleType = true;
+  if (cycleType == "Rerecord") recognizedCycleType = true;
+  if (cycleType == "Full") recognizedCycleType = true;
+  if (recognizedCycleType == false) EXIT_WITH_ERROR("[ERROR] Unrecognized cycle type '%s'");
+ 
   // Parsing configuration file
   nlohmann::json config;
   try { config = nlohmann::json::parse(configFileString); }
   catch (const std::exception &err) { EXIT_WITH_ERROR("[ERROR] Parsing configuration file %s. Details:\n%s\n", configFile.c_str(), err.what()); }
 
   // Getting initial state file from the configuration
-  auto initialStateFilePath = JSON_GET_STRING(config, "Initial State File Path");
+  const auto initialStateFilePath = JSON_GET_STRING(config, "Initial State File Path");
 
   // Getting emulator name from the configuration
-  auto emulatorName = JSON_GET_STRING(config, "Emulator");
+  const auto emulatorName = JSON_GET_STRING(config, "Emulator");
 
   // Getting emulator from its name and configuring it
   auto e = jaffarPlus::Emulator::getEmulator(emulatorName, JSON_GET_OBJECT(config, "Emulator Configuration"));
@@ -50,25 +64,45 @@ int main(int argc, char *argv[])
   // Initializing emulator
   e->initialize();
 
+  // Getting state size
+  const auto stateSize = e->getStateSize();
+
   // If initial state file defined, load it
   if (initialStateFilePath.empty() == false) e->loadStateFile(initialStateFilePath);
 
   // Getting input sequence
-  auto solutionSequence = jaffarPlus::split(solutionFileString, ' ');
+  const auto solutionSequence = jaffarPlus::split(solutionFileString, ' ');
 
   // Getting sequence length
-  auto sequenceLength = solutionSequence.size();
+  const auto sequenceLength = solutionSequence.size();
 
   // Printing information
   LOG("[J+] Emulator Name:          '%s'\n", e->getName().c_str());
   LOG("[J+] Config File:            '%s'\n", configFile.c_str());
   LOG("[J+] Solution File:          '%s'\n", solutionFile.c_str());
+  LOG("[J+] Cycle Type:             '%s'\n", cycleType.c_str());
+  LOG("[J+] State Size:              %lu\n", stateSize);
   LOG("[J+] Sequence Length:         %lu\n", sequenceLength);
-  LOG("[J+] Running...\n");
-  
-  // Playing input sequence
+  LOG("[J+] ********** Running Test **********\n");
+
+  // Serializing initial state
+  auto currentState = (uint8_t *)malloc(stateSize);
+  e->serializeState(currentState);
+
+  // Check whether to perform each action
+  const bool doPreAdvance = cycleType == "Full";
+  const bool doDeserialize = cycleType == "Rerecord" || cycleType == "Full";
+  const bool doSerialize = cycleType == "Rerecord" || cycleType == "Full";
+
+  // Actually running the sequence
   auto t0 = std::chrono::high_resolution_clock::now();
-  for (const auto& input : solutionSequence) e->advanceState(input);
+  for (const auto& input : solutionSequence)
+  {
+    if (doPreAdvance == true) e->advanceState(input);
+    if (doDeserialize == true) e->deserializeState(currentState);
+    e->advanceState(input);
+    if (doSerialize == true) e->serializeState(currentState);
+  }
   auto tf = std::chrono::high_resolution_clock::now();
 
   // Calculating running time
