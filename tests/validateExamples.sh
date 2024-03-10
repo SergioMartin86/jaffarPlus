2d00c30006561ffb4ebe50ea42b8eb5b8dd620a1
SergioMartin86 <eienburuu@gmail.com>
2024-03-10 11:19:49 +0100

    Adding script tester

--- tests/validateExamples.sh
+++ tests/validateExamples.sh
@@ -4,6 +4,14 @@ scriptFiles=`find ../examples -name "*.jaffar"`
 
 set -e
 
+# Override values
+export JAFFAR_OVERRIDE_RETURN_CODE=0
+export JAFFAR_DRIVER_OVERRIDE_DRIVER_MAX_STEP=1
+export JAFFAR_ENGINE_OVERRIDE_MAX_STATEDB_SIZE_MB=10
+export JAFFAR_QUICKERNES_OVERRIDE_ROM_FILE_SHA1="DB25BABE3ECB0203BF33A1FB2D0C06E22D87543C"
+export JAFFAR_QUICKERNES_OVERRIDE_ROM_FILE_PATH=`realpath ../emulators/quickerNES/testRom.nes`
+export JAFFAR_QUICKERNES_OVERRIDE_INITIAL_STATE_FILE_PATH=""
+
 for f in ${scriptFiles}
 do
  dirName=`dirname ${f}`
@@ -12,6 +20,7 @@ do
  echo ${fileName}
 
  pushd ${dirName}
-  JAFFAR_ENGINE_OVERRIDE_MAX_STATEDB_SIZE_MB=10 JAFFAR_DRIVER_OVERRIDE_DRIVER_MAX_STEP=1 JAFFAR_OVERRIDE_RETURN_CODE=1 jaffar ${fileName}
+  echo "Testing script file: ${f}"
+  jaffar ${fileName}
  popd
 done
