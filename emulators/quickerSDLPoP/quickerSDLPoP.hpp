5e3e63a95a1064d58f9c922b0d801f76cc50d050
SergioMartin86 <eienburuu@gmail.com>
2024-05-26 08:50:34 +0200

    Fixing style

--- emulators/quickerSDLPoP/quickerSDLPoP.hpp
+++ emulators/quickerSDLPoP/quickerSDLPoP.hpp
@@ -65,9 +65,9 @@ class QuickerSDLPoP final : public Emulator
 
   __INLINE__ void deserializeState(jaffarCommon::deserializer::Base &deserializer) override { _QuickerSDLPoP->deserializeState(deserializer); };
 
-  __INLINE__ void enableStateProperty(const std::string &property) override { }
+  __INLINE__ void enableStateProperty(const std::string &property) override {}
 
-  __INLINE__ void disableStateProperty(const std::string &property) override { }
+  __INLINE__ void disableStateProperty(const std::string &property) override {}
 
   __INLINE__ void loadFullState(const std::string &state) override
   {
