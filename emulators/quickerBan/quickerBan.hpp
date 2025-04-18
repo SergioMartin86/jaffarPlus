3a6f976f74b69727f9b7565d63a0df33be527c6c
SergioMartin86 <eienburuu@gmail.com>
2025-04-18 20:50:42 +0200

    Fixing tests

--- emulators/quickerBan/quickerBan.hpp
+++ emulators/quickerBan/quickerBan.hpp
@@ -32,12 +32,9 @@ class QuickerBan final : public Emulator
   // Function to get a reference to the input parser from the base emulator
   jaffar::InputParser *getInputParser() const override { return _quickerBan->getInputParser(); }
 
-  void initializeImpl() override
-  {
-    _quickerBan->initialize();
-  }
+  void initializeImpl() override { _quickerBan->initialize(); }
 
-  jaffar::EmuInstance* getEmulator() const { return _quickerBan.get(); }
+  jaffar::EmuInstance *getEmulator() const { return _quickerBan.get(); }
 
   // State advancing function
   void advanceStateImpl(const jaffar::input_t &input) override { _quickerBan->advanceState(input); }
@@ -45,25 +42,23 @@ class QuickerBan final : public Emulator
   __INLINE__ void serializeState(jaffarCommon::serializer::Base &serializer) const override { _quickerBan->serializeState(serializer); };
   __INLINE__ void deserializeState(jaffarCommon::deserializer::Base &deserializer) override { _quickerBan->deserializeState(deserializer); };
 
-  __INLINE__ void printInfo() const override { }
+  __INLINE__ void printInfo() const override {}
 
   property_t getProperty(const std::string &propertyName) const override
   {
     JAFFAR_THROW_LOGIC("Property name: '%s' not found in emulator '%s'", propertyName.c_str(), getName().c_str());
   }
 
-  void initializeVideoOutput() override {}
-  void finalizeVideoOutput() override { }
-  __INLINE__ void enableRendering() override {  }
-  __INLINE__ void disableRendering() override { }
-  __INLINE__ void updateRendererState(const size_t stepIdx, const std::string input) override { }
-  __INLINE__ void   serializeRendererState(jaffarCommon::serializer::Base &serializer) const override { }
-  __INLINE__ void   deserializeRendererState(jaffarCommon::deserializer::Base &deserializer) override { }
-  __INLINE__ size_t getRendererStateSize() const override { return 0;  }
+  void              initializeVideoOutput() override {}
+  void              finalizeVideoOutput() override {}
+  __INLINE__ void   enableRendering() override {}
+  __INLINE__ void   disableRendering() override {}
+  __INLINE__ void   updateRendererState(const size_t stepIdx, const std::string input) override {}
+  __INLINE__ void   serializeRendererState(jaffarCommon::serializer::Base &serializer) const override {}
+  __INLINE__ void   deserializeRendererState(jaffarCommon::deserializer::Base &deserializer) override {}
+  __INLINE__ size_t getRendererStateSize() const override { return 0; }
 
-  __INLINE__ void showRender() override
-  {
-  }
+  __INLINE__ void showRender() override {}
 
   private:
 
--- games/sokoban/sokoban.hpp
+++ games/sokoban/sokoban.hpp
@@ -21,14 +21,13 @@ class Sokoban final : public jaffarPlus::Game
 
   Sokoban(std::unique_ptr<Emulator> emulator, const nlohmann::json &config)
     : jaffarPlus::Game(std::move(emulator), config)
-  {
-  }
+  {}
 
   private:
 
   __INLINE__ void registerGameProperties() override
   {
-    _quickerBan = ((jaffarPlus::emulator::QuickerBan*)_emulator.get())->getEmulator();
+    _quickerBan = ((jaffarPlus::emulator::QuickerBan *)_emulator.get())->getEmulator();
 
     registerGameProperty("Can Move Up", &_canMoveUp, Property::datatype_t::dt_bool, Property::endianness_t::little);
     registerGameProperty("Can Move Down", &_canMoveDown, Property::datatype_t::dt_bool, Property::endianness_t::little);
@@ -65,47 +64,40 @@ class Sokoban final : public jaffarPlus::Game
     _isDeadlock = _quickerBan->getIsDeadlock();
   }
 
-  __INLINE__ void computeAdditionalHashing(MetroHash128 &hashEngine) const override
-  {
-    hashEngine.Update(_quickerBan->getState(), _quickerBan->getStateSize());
-  }
+  __INLINE__ void computeAdditionalHashing(MetroHash128 &hashEngine) const override { hashEngine.Update(_quickerBan->getState(), _quickerBan->getStateSize()); }
 
   // Updating derivative values after updating the internal state
   __INLINE__ void stateUpdatePostHook() override
   {
     _remainingBoxes = _quickerBan->getGoalCount() - _quickerBan->getBoxesOnGoal();
-    
-    // Getting pusher 
+
+    // Getting pusher
 
     // Checking if we can move up
     _canMoveUp = _quickerBan->canMoveUp();
     if (_hasMovedBox == false)
       if (*_pusherPosX == _pusherPrevPosX)
-        if ((*_pusherPosY-1) == _pusherPrevPosY) _canMoveUp = false;
+        if ((*_pusherPosY - 1) == _pusherPrevPosY) _canMoveUp = false;
 
     _canMoveDown = _quickerBan->canMoveDown();
     if (_hasMovedBox == false)
       if (*_pusherPosX == _pusherPrevPosX)
-        if ((*_pusherPosY+1) == _pusherPrevPosY) _canMoveDown = false;
+        if ((*_pusherPosY + 1) == _pusherPrevPosY) _canMoveDown = false;
 
     _canMoveLeft = _quickerBan->canMoveLeft();
     if (_hasMovedBox == false)
-      if ((*_pusherPosX-1) == _pusherPrevPosX)
+      if ((*_pusherPosX - 1) == _pusherPrevPosX)
         if (*_pusherPosY == _pusherPrevPosY) _canMoveLeft = false;
 
     _canMoveRight = _quickerBan->canMoveRight();
     if (_hasMovedBox == false)
-      if ((*_pusherPosX+1) == _pusherPrevPosX)
+      if ((*_pusherPosX + 1) == _pusherPrevPosX)
         if (*_pusherPosY == _pusherPrevPosY) _canMoveRight = false;
   }
 
-  __INLINE__ void ruleUpdatePreHook() override
-  {
-  }
+  __INLINE__ void ruleUpdatePreHook() override {}
 
-  __INLINE__ void ruleUpdatePostHook() override
-  {
-  }
+  __INLINE__ void ruleUpdatePostHook() override {}
 
   __INLINE__ void serializeStateImpl(jaffarCommon::serializer::Base &serializer) const override
   {
@@ -157,20 +149,20 @@ class Sokoban final : public jaffarPlus::Game
     return jaffarCommon::hash::hash_t();
   }
 
-  uint8_t* _pusherPosX;
-  uint8_t* _pusherPosY;
-  
-  bool _isDeadlock;
+  uint8_t *_pusherPosX;
+  uint8_t *_pusherPosY;
+
+  bool    _isDeadlock;
   uint8_t _pusherPrevPosX;
   uint8_t _pusherPrevPosY;
-  bool _hasMovedBox;
-  bool _canMoveUp;
-  bool _canMoveDown;
-  bool _canMoveLeft;
-  bool _canMoveRight;
+  bool    _hasMovedBox;
+  bool    _canMoveUp;
+  bool    _canMoveDown;
+  bool    _canMoveLeft;
+  bool    _canMoveRight;
 
-  uint16_t _remainingBoxes;
-  jaffar::EmuInstance* _quickerBan;
+  uint16_t             _remainingBoxes;
+  jaffar::EmuInstance *_quickerBan;
 };
 
 } // namespace sokoban
