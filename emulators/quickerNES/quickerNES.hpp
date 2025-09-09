a4269afda10a84d6296bb66c02abdef87b2ad409
SergioMartin86 <eienburuu@gmail.com>
2025-09-09 20:19:02 +0200

    Progress

--- emulators/quickerNES/quickerNES.hpp
+++ emulators/quickerNES/quickerNES.hpp
@@ -32,6 +32,12 @@ public:
   // Constructor must only do configuration parsing
   QuickerNES(const nlohmann::json& config) : Emulator(config)
   {
+    // Specifies how to map code ROM
+    // Flat: The game program memory is placed raw in a contiguous array. This is faster for instruction decoding but every page switch triggers a full copy
+    // Paged: The game program memory is properly distributed in pages whose address is determined by pointers. The indirection adds cost to instruction decoding but does not require a copy on switch
+    // Use flat for games or sections that do not perform frequent bank switching; paged, otherwise
+    _useFlatCodeMap = jaffarCommon::json::getBoolean(config, "Use Flat Code Map");
+    
     // Getting initial state file from the configuration
     _initialStateFilePath = jaffarCommon::json::getString(config, "Initial State File Path");
 
@@ -66,6 +72,9 @@ public:
     // Getting disabled state properties
     const auto disabledStateProperties = jaffarCommon::json::getArray<std::string>(config, "Disabled State Properties");
     for (const auto& property : disabledStateProperties) _disabledStateProperties.push_back(property);
+
+    // Storing whether to use a flat code map
+    _initialStateFilePath = jaffarCommon::json::getString(config, "Initial State File Path");
 
     // Creating internal emulator instance
     _quickerNES = std::make_unique<NESInstance>(config);
@@ -144,6 +153,9 @@ public:
       // Pushing data into RAM
       memcpy(_quickerNES->getLowMem(), initialRAMDataString.data(), 0x800);
     }
+    
+    // Flattening code map, if required
+    if (_useFlatCodeMap == true) _quickerNES->useFlatCodeMap();
   }
 
   // State advancing function
@@ -155,6 +167,7 @@ public:
   __INLINE__ void printInfo() const override
   {
    jaffarCommon::logger::log("[J+]  + Initial Sequence File Path:          '%s'\n", _initialSequenceFilePath.c_str()); 
+   jaffarCommon::logger::log("[J+]  + Uses Flat Code Map:                  %s\n", _useFlatCodeMap ? "True" : "False"); 
   }
 
   property_t getProperty(const std::string& propertyName) const override
@@ -321,6 +334,7 @@ private:
 
   std::unique_ptr<NESInstance> _quickerNES;
 
+  bool        _useFlatCodeMap;
   size_t      _NTABBlockSize;
   size_t      _SRAMBlockSize;
   std::string _romFilePath;
