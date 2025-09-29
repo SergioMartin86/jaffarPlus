3d15224a059c39a18ecad84a4cf57b60e7075344
Sergio Martin <eienburuu@gmail.com>
2025-09-29 16:57:21 +0000

    Updating smb

--- games/nes/superMarioBros.hpp
+++ games/nes/superMarioBros.hpp
@@ -18,7 +18,32 @@ class SuperMarioBros final : public jaffarPlus::Game
 public:
   static __INLINE__ std::string getName() { return "NES / Super Mario Bros"; }
 
-  SuperMarioBros(std::unique_ptr<Emulator> emulator, const nlohmann::json& config) : jaffarPlus::Game(std::move(emulator), config) {}
+  SuperMarioBros(std::unique_ptr<Emulator> emulator, const nlohmann::json& config) : jaffarPlus::Game(std::move(emulator), config)
+  {
+    _traceFilePath = jaffarCommon::json::getString(config, "Trace File Path");
+
+    // Loading trace
+    if (_traceFilePath != "")
+    {
+      _useTrace = true;
+      std::string traceData;
+      bool        status = jaffarCommon::file::loadStringFromFile(traceData, _traceFilePath.c_str());
+      if (status == false) JAFFAR_THROW_LOGIC("Could not find/read trace file: %s\n", _traceFilePath.c_str());
+
+      std::istringstream f(traceData);
+      std::string line;
+      while (std::getline(f, line))
+      {
+        auto coordinates = jaffarCommon::string::split(line, ' ');
+        float x = std::atof(coordinates[0].c_str());
+        float y = std::atof(coordinates[1].c_str());
+        _trace.push_back(traceEntry_t{
+          .x = x,
+          .y = y,
+        });
+      }
+    }
+  }
 
 private:
   __INLINE__ void registerGameProperties() override
@@ -181,12 +206,16 @@ private:
     _playerPosY = 0;
     _screenPosX = 0;
     _playerScreenOffset = 0;
+    _currentStep = 0;
   }
 
   __INLINE__ void advanceStateImpl(const InputSet::inputIndex_t input) override
   {
     // Running emulator
     _emulator->advanceState(input);
+
+    // Advancing current step
+    _currentStep++;
   }
 
   __INLINE__ void computeAdditionalHashing(MetroHash128& hashEngine) const override
@@ -197,23 +226,23 @@ private:
     hashEngine.Update(*_playerState);
     hashEngine.Update(*_playerPosX1);
     hashEngine.Update(*_playerPosX2);
-  //    hashEngine.Update(*_playerPosX3);
+    hashEngine.Update(*_playerPosX3);
     hashEngine.Update(*_playerPosY2);
-  //    hashEngine.Update(*_playerPosY2);
+    hashEngine.Update(*_playerPosY2);
     hashEngine.Update(*_playerMomentumX);
     hashEngine.Update(*_playerFacingDirection);
     hashEngine.Update(*_playerMovingDirection);
     hashEngine.Update(*_playerFloatingMode);
     hashEngine.Update(*_playerWalkingMode);
-  //    hashEngine.Update(*_playerWalkingDelay);
-  //    hashEngine.Update(*_playerWalkingFrame);
-  //    hashEngine.Update(*_playerMaxVelLeft);
-  //    hashEngine.Update(*_playerMaxVelRight);
+    hashEngine.Update(*_playerWalkingDelay);
+    hashEngine.Update(*_playerWalkingFrame);
+    hashEngine.Update(*_playerMaxVelLeft);
+    hashEngine.Update(*_playerMaxVelRight);
     hashEngine.Update(*_playerVelX1);
     hashEngine.Update(*_playerVelY1);
     hashEngine.Update(*_playerVelY2);
-  //    hashEngine.Update(*_playerGravity);
-  //    hashEngine.Update(*_playerFriction);
+    hashEngine.Update(*_playerGravity);
+    hashEngine.Update(*_playerFriction);
 
     hashEngine.Update(*_screenPosX2);
     hashEngine.Update(*_screenPosX3);
@@ -283,6 +312,11 @@ private:
     _pointMagnet.intensity = 0.0;
     _pointMagnet.x         = 0.0;
     _pointMagnet.y         = 0.0;
+
+    _traceMagnet.intensityX = 0.0;
+    _traceMagnet.intensityY = 0.0;
+    _traceMagnet.offset = 0;
+
   }
 
   __INLINE__ void ruleUpdatePostHook() override
@@ -291,10 +325,27 @@ private:
     _playerDistanceToPointX = std::abs(_pointMagnet.x - _playerPosX);
     _playerDistanceToPointY = std::abs(_pointMagnet.y - _playerPosY);
     _playerDistanceToPoint  = sqrtf(_playerDistanceToPointX * _playerDistanceToPointX + _playerDistanceToPointY * _playerDistanceToPointY);
+
+      // Updating trace stuff
+    if (_useTrace == true)
+    {
+      _traceStep = (uint16_t) std::max(std::min( (int)_currentStep + _traceMagnet.offset, (int) _trace.size() - 1), 0);
+      _traceTargetX = _trace[_traceStep].x;
+      _traceTargetY = _trace[_traceStep].y;
+
+      // Updating distance to trace point
+      _traceDistanceX = std::abs(_traceTargetX - _playerPosX);
+      _traceDistanceY = std::abs(_traceTargetY - _playerPosY);
+
+      if (_traceDistanceX > 100) _traceDistanceX = 10.0;
+      if (_traceDistanceY > 100) _traceDistanceX = 10.0;
+      _traceDistance  = sqrtf(_traceDistanceX * _traceDistanceX + _traceDistanceY * _traceDistanceY);
+    }
   }
 
   __INLINE__ void serializeStateImpl(jaffarCommon::serializer::Base& serializer) const override
   {
+     serializer.push(&_currentStep, sizeof(_currentStep));
      serializer.push(&_playerPosX, sizeof(_playerPosX));
      serializer.push(&_playerPosY, sizeof(_playerPosY));
      serializer.push(&_screenPosX, sizeof(_screenPosX));
@@ -303,6 +354,7 @@ private:
 
   __INLINE__ void deserializeStateImpl(jaffarCommon::deserializer::Base& deserializer)
   {
+     deserializer.pop(&_currentStep, sizeof(_currentStep));
      deserializer.pop(&_playerPosX, sizeof(_playerPosX));
      deserializer.pop(&_playerPosY, sizeof(_playerPosY));
      deserializer.pop(&_screenPosX, sizeof(_screenPosX));
@@ -314,6 +366,10 @@ private:
     // Getting rewards from rules
     float reward = 0.0;
 
+    // If trace is used, compute its magnet's effect
+    if (_useTrace == true)  reward += -1.0 * _traceMagnet.intensityX * _traceDistanceX;
+    if (_useTrace == true)  reward += -1.0 * _traceMagnet.intensityY * _traceDistanceY;
+
     // Distance to point magnet
     reward += -1.0 * _pointMagnet.intensity * _playerDistanceToPoint;
 
@@ -323,6 +379,7 @@ private:
 
   void printInfoImpl() const override
   {
+    jaffarCommon::logger::log("[J+]  + Current Step:            %04u\n", _currentStep);
     jaffarCommon::logger::log("[J+]  + Current World-Stage:     %1u-%1u (Base Values: %2u - %2u)\n", *_currentWorld + 1, *_currentStage + 1, *_currentWorld, *_currentStage);
     jaffarCommon::logger::log("[J+]  + Time Left:               %1u%1u%1u\n", *_timeLeft100, *_timeLeft10, *_timeLeft1);
     jaffarCommon::logger::log("[J+]  + Player Animation:        %02u\n", *_playerAnimation);
@@ -357,6 +414,17 @@ private:
       jaffarCommon::logger::log("[J+]    + Distance Y                             %3.3f\n", _playerDistanceToPointY);
       jaffarCommon::logger::log("[J+]    + Total Distance                         %3.3f\n", _playerDistanceToPoint);
     }
+
+    if (_useTrace == true)
+    {
+      if (std::abs(_traceMagnet.intensityX) > 0.0f || std::abs(_traceMagnet.intensityY) > 0.0f)
+      {
+        jaffarCommon::logger::log("[J+]  + Trace Magnet                             Intensity: (X: %.5f, Y: %.5f), Step: %u (%+1u), X: %3.3f, Y: %3.3f\n", _traceMagnet.intensityX, _traceMagnet.intensityY, _traceStep, _traceMagnet.offset, _traceTargetX, _traceTargetY);
+        jaffarCommon::logger::log("[J+]    + Distance X                             %3.3f\n", _traceDistanceX);
+        jaffarCommon::logger::log("[J+]    + Distance Y                             %3.3f\n", _traceDistanceY);
+        jaffarCommon::logger::log("[J+]    + Total Distance                         %3.3f\n", _traceDistance);
+      }
+    }
   }
 
   bool parseRuleActionImpl(Rule& rule, const std::string& actionType, const nlohmann::json& actionJs) override
@@ -369,6 +437,16 @@ private:
       auto x         = jaffarCommon::json::getNumber<float>(actionJs, "X");
       auto y         = jaffarCommon::json::getNumber<float>(actionJs, "Y");
       rule.addAction([=, this]() { this->_pointMagnet = pointMagnet_t{.intensity = intensity, .x = x, .y = y}; });
+      recognizedActionType = true;
+    }
+
+    if (actionType == "Set Trace Magnet")
+    {
+      if (_useTrace == false) JAFFAR_THROW_LOGIC("Specified Trace Magnet, but no trace file was provided.");
+      auto intensityX = jaffarCommon::json::getNumber<float>(actionJs, "Intensity X");
+      auto intensityY = jaffarCommon::json::getNumber<float>(actionJs, "Intensity Y");
+      auto offset    = jaffarCommon::json::getNumber<int>(actionJs, "Offset");
+      rule.addAction([=, this]() { this->_traceMagnet = traceMagnet_t{.intensityX = intensityX, .intensityY = intensityY, .offset = offset }; });
       recognizedActionType = true;
     }
 
@@ -475,6 +553,74 @@ private:
   float _playerPosY;
   float _screenPosX;
   float _playerScreenOffset;
+
+  uint16_t _currentStep;
+
+  ///////////// Tracing stuff
+
+  bool _isDumpingTrace = false;
+  std::string _traceDumpString;
+
+  __INLINE__ void playerPrintCommands() const override
+  {
+    jaffarCommon::logger::log("[J+] t: start/stop trace dumping (%s)\n", _isDumpingTrace ? "On" : "Off");
+  };
+
+  __INLINE__ bool playerParseCommand(const int command)
+  {
+    // If storing a trace, do it here
+    if (_isDumpingTrace == true) _traceDumpString += std::to_string(_playerPosX) + std::string(" ") + std::to_string(_playerPosY) + std::string("\n");
+
+     if (command == 't')
+     {
+      if (_isDumpingTrace == false)
+      {
+        _isDumpingTrace = true;
+        _traceDumpString = "";
+        return false;
+      }
+      else
+      {
+        const std::string dumpOutputFile = "jaffar.trace";
+        jaffarCommon::logger::log("[J+] Dumping trace to file: '%s'", dumpOutputFile.c_str());
+        jaffarCommon::file::saveStringToFile(_traceDumpString, dumpOutputFile);
+        _isDumpingTrace = false;
+        return true;
+      }
+     }
+
+     return false;
+  };
+
+  // Whether we use a trace
+  bool _useTrace = false;
+
+  struct traceMagnet_t
+  {
+    float intensityX = 0.0; // How strong the magnet is on X
+    float intensityY = 0.0; // How strong the magnet is on Y
+    int offset      = 0; // Which entry (step) to look at wrt the current emulation step
+  };
+
+  // Trace contents
+  struct traceEntry_t
+  {
+    float x;
+    float y;
+  };
+  std::vector<traceEntry_t> _trace;
+
+  // Location of the trace file
+  traceMagnet_t _traceMagnet;
+  std::string _traceFilePath;
+
+  // Current trace target
+  uint16_t _traceStep;
+  float _traceTargetX;
+  float _traceTargetY;
+  float _traceDistanceX;
+  float _traceDistanceY;
+  float _traceDistance;
 
   // Pointer to emulator's low memory storage
   uint8_t* _lowMem;
